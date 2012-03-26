8e67bc9d3bce6b9674e31262efdbbe7176d3c61b
ben7th <ben7th@sina.com>
2012-03-26 17:01:35 +0800

    投票前端集成：列表页面

--- apps/pin-edu-sns/app/helpers/vote_helper.rb
+++ apps/pin-edu-sns/app/helpers/vote_helper.rb
@@ -6,4 +6,10 @@ module VoteHelper
     return "限选#{vote.select_limit}项"
   end
   
+  # 根据参与人数返回描述字符串
+  def voted_users_count_str(vote)
+    count = vote.voted_users.count
+    count == 0 ? '还没有人参与' : "#{count}人参与"
+  end
+  
 end
