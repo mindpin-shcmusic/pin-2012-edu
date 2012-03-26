17289feb1be83d554a4b17d0ddac35dd31b0889c
ben7th <ben7th@sina.com>
2012-03-26 13:06:01 +0800

    bugfix：找不到flash_info方法

--- lib/auto_loads/helpers/mindpin_layout_helper.rb
+++ lib/auto_loads/helpers/mindpin_layout_helper.rb
@@ -7,4 +7,15 @@ module MindpinLayoutHelper
     end
   end
   
+  # 回显校验信息
+  # TODO rails3 下，需要重写
+  def flash_info
+    re = []
+    [:notice, :error, :success].each do |kind|
+      msg = flash[kind]
+      re << "<div class='flash-#{kind}'><span>#{msg}</span></div>" if !msg.blank?
+    end
+    re*''
+  end
+  
 end
