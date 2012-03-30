0de73b45592bca4ed2bc618e0f5bf95992a97db9
ben7th <ben7th@sina.com>
2012-03-30 13:51:46 +0800

    admin前端

--- apps/pin-admin/script/script/import_students_and_teachers_and_users.rb
+++ apps/pin-admin/script/script/import_students_and_teachers_and_users.rb
@@ -88,5 +88,41 @@ ActiveRecord::Base.transaction do
     
   end
 end
+
+ActiveRecord::Base.transaction do
+  
+  raw_count = 0
+  CSV.open(File.join(Rails.root,"/tmp/teams.csv"), 'r') do |row|
+    raw_count+=1
+    
+    begin
+      name = row[0]
+      cid = row[1]
+      
+      tid = cid.gsub("ys","t")
+      teacher = Teacher.find_by_tid(tid)
+      
+      team = Team.new(
+        :name=>name,
+        :cid=>cid,
+        :teacher=>teacher
+      )
+      team.save!
+      
+      index = cid.gsub("ys","").to_i
+      student = Student.find_by_sid("s01")
+      first_id = student.id+((index-1)*5)
+      student_ids = [first_id,first_id+1,first_id+2,first_id+3,first_id+4]
+      team.student_ids = student_ids 
+      
+    rescue Exception=>ex
+      p ex.message
+      puts ex.backtrace*"\n"
+      raise "第 #{raw_count}行解析出错,本次操作已经回滚"
+    end
+    
+  end
+end
+
   
 
