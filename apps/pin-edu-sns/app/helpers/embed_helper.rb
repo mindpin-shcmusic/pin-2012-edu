ed443b96410a0319bd6902a520c4e5fdf7d57533
ben7th <ben7th@sina.com>
2012-04-28 07:38:02 +0800

    视频上传，pjax

--- apps/pin-edu-sns/app/helpers/embed_helper.rb
+++ apps/pin-edu-sns/app/helpers/embed_helper.rb
@@ -1,21 +1,22 @@
 module EmbedHelper
-  def video_tag(src)
-    %`
-    <embed 
-      height="356" 
-      allowscriptaccess="never" 
-      style="visibility: visible;" 
-      pluginspage="http://get.adobe.com/cn/flashplayer/" 
-      flashvars="" 
-      width="440" 
-      allowfullscreen="true" 
-      quality="hight" 
-      src="#{src}"
-      type="application/x-shockwave-flash" 
-      wmode="opaque"
-      data-pinit="registered">
+  def video_tag(media_file)
+    %~
+      <embed 
+        height='356' 
+        allowscriptaccess='never' 
+        style='visibility:visible;' 
+        pluginspage='http://get.adobe.com/cn/flashplayer/' 
+        flashvars='' 
+        width='440' 
+        allowfullscreen='true' 
+        quality='hight' 
+        src="#{media_file.swf_player_url}"
+        type='application/x-shockwave-flash' 
+        wmode='opaque'
+        data-pinit='registered'
+      >
       </embed>
-    `
+    ~.html_safe
 
   end
 end
