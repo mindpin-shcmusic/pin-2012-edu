1c363dc663d508afff9e616b23656b7b8c4a9629
ben7th <ben7th@sina.com>
2012-08-02 09:57:06 +0800

    修改索引文件

--- pin-edu-sns/lib/redis_related/dbs/redis_database.rb
+++ pin-edu-sns/lib/redis_related/dbs/redis_database.rb
@@ -1,15 +1,18 @@
 class RedisDatabase
-  QUEUE_DB = 0
-  TIP_DB   = 1
+  # DB 只能取 0 - 15 这十六个数字之一
   
   if Rails.env.test?
-    CACHE_DB = 3
+    TIP_DB   = 8
+    QUEUE_DB = 9
+    CACHE_DB = 10
   else
+    TIP_DB   = 0
+    QUEUE_DB = 1
     CACHE_DB = 2
   end
 
   def self.get_db_instance(db)
-    redis = Redis.new(:thread_safe=>true)
+    redis = Redis.new(:thread_safe => true)
     redis.select(db)
     redis
   end
