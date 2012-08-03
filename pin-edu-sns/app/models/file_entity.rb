34d65a6e3470f12c272191c19b46f7d4c5397704
ben7th <ben7th@sina.com>
2012-08-03 18:24:58 +0800

    增加图片style

--- pin-edu-sns/app/models/file_entity.rb
+++ pin-edu-sns/app/models/file_entity.rb
@@ -2,19 +2,21 @@ class FileEntity < ActiveRecord::Base
   has_many :media_resources
 
   has_attached_file :attach,
-    :path => R::FILE_ENTITY_ATTACHED_PATH,
-    :url  => R::FILE_ENTITY_ATTACHED_URL
+                    :styles => {
+                      :large => '460x340#',
+                      :small => '220x140#'
+                    },
+                    :path => R::FILE_ENTITY_ATTACHED_PATH,
+                    :url  => R::FILE_ENTITY_ATTACHED_URL
 
   def self.get_or_greate_by_file_md5(file)
     md5 = Digest::MD5.file(file).to_s
 
-    entity = find_by_md5 md5
-    if entity
-      return entity
-    end
+    entity = self.find_by_md5 md5
+    return entity if entity
 
-    entity = FileEntity.create :attach => file,
-                               :md5    => md5
+    return FileEntity.create :attach => file,
+                             :md5 => md5
   end
 
   ##################
