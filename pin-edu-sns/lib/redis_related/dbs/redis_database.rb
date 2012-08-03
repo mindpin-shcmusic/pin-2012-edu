17a898b8676970c0f1fc8036a9b86d0f435a9539
fushang318 <fushang318@gmail.com>
2012-08-03 16:14:55 +0800

    删除费代码

--- pin-edu-sns/lib/redis_related/dbs/redis_database.rb
+++ pin-edu-sns/lib/redis_related/dbs/redis_database.rb
@@ -1,12 +1,14 @@
 class RedisDatabase
   QUEUE_DB = 0
   TIP_DB   = 1
+  SEARCH_DB = 4
   
   if Rails.env.test?
     CACHE_DB = 3
   else
     CACHE_DB = 2
   end
+
 
   def self.get_db_instance(db)
     redis = Redis.new(:thread_safe=>true)
