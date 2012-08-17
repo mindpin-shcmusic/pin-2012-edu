d88eca8778720d9b66c89b645ca59a03425063a5
fushang318 <fushang318@gmail.com>
2012-08-17 17:35:00 +0800

    去掉 slice_temp_file 相关常量

--- pin-edu-sns/lib/r.rb
+++ pin-edu-sns/lib/r.rb
@@ -4,6 +4,4 @@ class R
 
   FILE_ENTITY_ATTACHED_PATH     = "#{MINDPIN_MRS_DATA_PATH}:url"
   FILE_ENTITY_ATTACHED_URL      = "/attachments/:attachment/:id/:style/:filename"
-
-  SLICE_TEMP_FILE_ATTACHED_DIR  = "#{MINDPIN_MRS_DATA_PATH}/attachments/slice_temp_files/"
 end
