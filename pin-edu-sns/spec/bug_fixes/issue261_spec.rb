6b4a61630f0c3bf7fb00e79da36600b3c10e58d9
Kaid <kaid@kaid.me>
2012-08-24 10:47:08 +0800

    issue261 fixed

--- pin-edu-sns/spec/bug_fixes/issue261_spec.rb
+++ pin-edu-sns/spec/bug_fixes/issue261_spec.rb
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
-describe '老师分配作业给学生B所在班级后B学生被管理员删除，查看作业状况时不应该再出现B学生' do
-  before {
-    homework = FactoryGirl.create(:homework)
-    homework.assign_to
-  }
+describe 'issue261' do
+  let(:team) {FactoryGirl.create :team, :with_student_users}
+  let(:homework) {FactoryGirl.create(:homework)}
+  let(:student_user) {team.student_users.first}
+
+  before(:all) do
+    homework.assign_to :teams => [team.id]
+    homework.homework_assign_rule.build_assign
+  end
+
+  it '如果删除学生B，老师在查看作业状况时不应该再出现学生B' do
+    homework.assignees.should include student_user
+    student_user.student.destroy
+    homework.assignees.reload.should_not include student_user
+  end
 end
