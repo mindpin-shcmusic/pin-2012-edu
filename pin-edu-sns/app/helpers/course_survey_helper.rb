747021e49a32f4657ed2545f37e5352313ce1563
mykingla <kingla_pei@163.com>
2012-10-08 11:14:04 +0800

    Use group_by

--- pin-edu-sns/app/helpers/course_survey_helper.rb
+++ pin-edu-sns/app/helpers/course_survey_helper.rb
@@ -25,8 +25,102 @@ module CourseSurveyHelper
     teaching_result_good, teaching_result_general, teaching_result_bad = 0, 0, 0
     result_reason_teacher, result_reason_student = 0, 0
 
+    records = course_survey.course_survey_records
 
-    course_survey.course_survey_records.each do |record|
+    records_group = records.group_by(&:on_off_class)
+    on_off_class = records_group['on_off_class'].length if !records_group['on_off_class'].nil?
+
+    records_group = records.group_by(&:checking_institution)
+    checking_institution = records_group['checking_institution'].length if !records_group['checking_institution'].nil?
+
+
+    records_group = records.group_by(&:class_order)
+    class_order = records_group['class_order'].length if !records_group['class_order'].nil?
+
+
+    records_group = records.group_by(&:prepare_situation)
+    prepare_situation_total_enough = records_group['TOTAL_ENOUGH'].length if !records_group['TOTAL_ENOUGH'].nil?
+    prepare_situation_general_enough = records_group['GENERAL_ENOUGH'].length if !records_group['GENERAL_ENOUGH'].nil?
+    prepare_situation_not_enough = records_group['NOT_ENOUGH'].length if !records_group['NOT_ENOUGH'].nil?
+
+
+    records_group = records.group_by(&:teaching_level)
+    teaching_level_good = records_group['GOOD'].length if !records_group['GOOD'].nil?
+    teaching_level_general = records_group['GENERAL'].length if !records_group['GENERAL'].nil?
+
+
+    records_group = records.group_by(&:teacher_morality)
+    teacher_morality_good = records_group['GOOD'].length if !records_group['GOOD'].nil?
+    teacher_morality_better = records_group['BETTER'].length if !records_group['BETTER'].nil?
+
+
+    records_group = records.group_by(&:class_content)
+    class_content_rich_relation = records_group['RICH_RELATION'].length if !records_group['RICH_RELATION'].nil?
+    class_content_general_relation = records_group['GENERAL_RELATION'].length if !records_group['GENERAL_RELATION'].nil?
+
+
+    records_group = records.group_by(&:knowledge_level)
+    knowledge_level_rich = records_group['RICH'].length if !records_group['RICH'].nil?
+    knowledge_level_richer = records_group['RICHER'].length if !records_group['RICHER'].nil?
+
+
+    records_group = records.group_by(&:teaching_schedule)
+    teaching_schedule_reasonable = records_group['REASONABLE'].length if !records_group['REASONABLE'].nil?
+    teaching_schedule_basic_reasonable = records_group['BASIC_REASONABLE'].length if !records_group['BASIC_REASONABLE'].nil?
+    teaching_schedule_not_reasonable = records_group['NOT_REASONABLE'].length if !records_group['NOT_REASONABLE'].nil?
+
+
+    records_group = records.group_by(&:teaching_interact)
+    teaching_interact_good = records_group['GOOD'].length if !records_group['GOOD'].nil?
+    teaching_interact_better = records_group['BETTER'].length if !records_group['BETTER'].nil?
+
+
+    records_group = records.group_by(&:board_writing_quality)
+    board_writing_quality_good = records_group['GOOD'].length if !records_group['GOOD'].nil?
+    board_writing_quality_general = records_group['GENERAL'].length if !records_group['GENERAL'].nil?
+
+
+    records_group = records.group_by(&:has_courseware)
+    has_courseware_yes = records_group['has_courseware'].length if !records_group['has_courseware'].nil?
+    has_courseware_yes = records_group['has_courseware'].length if !records_group['has_courseware'].nil?
+
+
+    records_group = records.group_by(&:has_courseware)
+    has_courseware_yes = records_group['YES'].length if !records_group['YES'].nil?
+    has_courseware_no = records_group['NO'].length if !records_group['NO'].nil?
+
+    records_group = records.group_by(&:courseware_quality)
+    courseware_quality_good = records_group['GOOD'].length if !records_group['GOOD'].nil?
+    courseware_quality_general = records_group['GENERAL'].length if !records_group['GENERAL'].nil?
+
+
+    records_group = records.group_by(&:speak_level)
+    speak_level_good = records_group['speak_level'].length if !records_group['speak_level'].nil?
+    speak_level_general = records_group['speak_level'].length if !records_group['speak_level'].nil?
+
+
+    records_group = records.group_by(&:speak_level)
+    speak_level_good = records_group['GOOD'].length if !records_group['GOOD'].nil?
+    speak_level_general = records_group['GENERAL'].length if !records_group['GENERAL'].nil?
+
+    records_group = records.group_by(&:study_result)
+    study_result_much = records_group['MUCH'].length if !records_group['MUCH'].nil?
+    study_result_little = records_group['LITTLE'].length if !records_group['LITTLE'].nil?
+    study_result_none = records_group['NONE'].length if !records_group['NONE'].nil?
+
+
+    records_group = records.group_by(&:teaching_result)
+    teaching_result_good = records_group['GOOD'].length if !records_group['GOOD'].nil?
+    teaching_result_general = records_group['GENERAL'].length if !records_group['GENERAL'].nil?
+    teaching_result_bad = records_group['BAD'].length if !records_group['BAD'].nil?
+
+    records_group = records.group_by(&:result_reason)
+    result_reason_teacher = records_group['TEACHER_REASON'].length if !records_group['TEACHER_REASON'].nil?
+    result_reason_student = records_group['STUDENT_REASON'].length if !records_group['STUDENT_REASON'].nil?
+
+=begin
+
+    records.each do |record|
 
       on_off_class += 1 if record.on_off_class
 
@@ -152,7 +246,7 @@ module CourseSurveyHelper
         result_reason_student += 1
       end
     end
-
+=end
 
     
     {
