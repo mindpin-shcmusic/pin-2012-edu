375eeef9ddf51047117b59911042c9a0a4d8d9ca
mykingla <kingla_pei@163.com>
2012-10-09 18:14:49 +0800

    Add callback in question.rb

--- pin-edu-sns/app/models/question.rb
+++ pin-edu-sns/app/models/question.rb
@@ -14,6 +14,25 @@ class Question < ActiveRecord::Base
   scope :unanswered, where(:has_answered => false)
 
 
+  after_create :send_tip_message_for_receiver_on_create
+  def send_tip_message_for_receiver_on_create
+    receiver = self.teacher_user
+
+    receiver.question_tip_message.put("#{self.creator.name} 给你发了问题", self.id)
+    receiver.question_tip_message.send_count_to_juggernaut
+  end
+
+  after_destroy :send_tip_message_on_destroy
+  def send_tip_message_on_destroy
+    receiver = self.teacher_user
+
+    if !receiver.blank?
+      receiver.question_tip_message.delete(self.id)
+      receiver.question_tip_message.send_count_to_juggernaut
+    end
+  end
+
+
 
   module UserMethods
     def self.included(base)
