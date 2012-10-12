a8673bfc9a47edefcb9a2ceb65956df8a39d445e
fushang318 <fushang318@gmail.com>
2012-10-12 17:32:47 +0800

    移动资源相关模型

--- pin-edu-sns/app/models/aliyun_multipart_upload_part.rb
+++
@@ -1,8 +0,0 @@
-class AliyunMultipartUploadPart < ActiveRecord::Base
-  belongs_to :file_entity
-
-  validates :file_entity_id, :presence => true
-  validates :upload_id, :presence => true, :uniqueness => {:scope => :file_entity_id}
-  validates :part_num,  :presence => true, :uniqueness => {:scope => [:file_entity_id,:upload_id]}
-  validates :md5, :presence => true
-end
