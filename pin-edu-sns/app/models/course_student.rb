99efcc8fd952b55500081c278db30b1f00152c7b
fushang318 <fushang318@gmail.com>
2012-10-12 17:38:28 +0800

    移动课程相关模型

--- pin-edu-sns/app/models/course_student.rb
+++
@@ -1,6 +0,0 @@
-class CourseStudent < ActiveRecord::Base
-  belongs_to :course
-  belongs_to :student_user, :class_name => 'User', :foreign_key => :student_user_id
-
-  validates :course, :student_user, :presence => true
-end
