92bdd58c535c2a23a70ad962eff9e7f82762f835
fushang318 <fushang318@gmail.com>
2012-10-17 18:09:33 +0800

    course_survey.has_permission?(current_user)

--- pin-edu-sns/app/edu_models/survey_pack/course_survey.rb
+++ pin-edu-sns/app/edu_models/survey_pack/course_survey.rb
@@ -1,9 +1,18 @@
 class CourseSurvey < ActiveRecord::Base
   has_many :course_survey_records, :dependent => :destroy
   has_many :course_survey_es_records, :dependent => :destroy
+  belongs_to :course_teacher
 
   validates :title, :presence => true
 
   scope :with_kind, lambda {|kind| {:conditions => ['kind = ?', kind]}}
 
+  def has_permission?(student_user)
+    return false if !student_user.is_student?
+
+    course_teacher.course.get_students(
+      :semester => course_teacher.semester,
+      :teacher_user => course_teacher.teacher_user
+    ).include?(student_user)
+  end
 end
