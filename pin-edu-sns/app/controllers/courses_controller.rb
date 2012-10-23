8686a5a62c2d8add0e7d8792f22e812054311e3e
mykingla <kingla_pei@163.com>
2012-10-23 15:52:35 +0800

    []

--- pin-edu-sns/app/controllers/courses_controller.rb
+++ pin-edu-sns/app/controllers/courses_controller.rb
@@ -30,35 +30,15 @@ class CoursesController < ApplicationController
   # 从现在时间开始，本周内上的课程，包括当前正在进行的课程
   def next_for_student
     @next_course_teachers = []
-    
 
     current_cte = CourseTimeExpression.get_by_time(Time.now)
 
     student_courses = current_user.get_student_course_teachers(:semester => Semester.now)
     student_courses.each do |course_teacher|
-      time_expression = JSON.parse(course_teacher.time_expression)
-      time_expression.each do |expression|
-        
-        expression['number'].each do |number|
-          cte = CourseTimeExpression.new(expression['weekday'], [number])
-          if cte >= current_cte
-            class_detail = Hash.new(0)
-
-            class_detail[:weekday] = cte.weekday
-            class_detail[:weekday_str] = cte.weekday_str
-            class_detail[:class_time] = "#{cte.start_time_str} - #{cte.end_time_str}"
-            class_detail[:course_teacher] = course_teacher
-
-            @next_course_teachers << class_detail
-          end
-        end
-        
-      end
- 
+      @next_course_teachers << course_teacher.get_next_courses_by_time_expression(current_cte)
     end
 
-
-    @next_course_teachers = @next_course_teachers.sort_by {|class_detail| class_detail[:weekday]}
+    # @next_course_teachers = @next_course_teachers.sort_by {|class_detail| class_detail[:weekday]}
     @next_course_teachers = @next_course_teachers.group_by{|item| item[:weekday]}
 
   end
