bad7787dc172d891c6c6833b41562f062bd0fc0e
fushang318 <fushang318@gmail.com>
2012-10-29 10:51:15 +0800

    fix 教学计划 创建

--- pin-edu-sns/app/edu_models/course_pack/teaching_plan_course.rb
+++ pin-edu-sns/app/edu_models/course_pack/teaching_plan_course.rb
@@ -1,8 +1,6 @@
 class TeachingPlanCourse < ActiveRecord::Base
 
   validates :course_id, :presence => true
-  validates :teaching_plan_id, :presence => true,
-    :uniqueness => {:scope => :course_id}
 
   belongs_to :course
   belongs_to :teaching_plan
