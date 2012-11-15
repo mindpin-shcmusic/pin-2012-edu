8d57f6eab2db98ba84afc282f66c09037a5e815c
Kaid <kaid@kaid.me>
2012-11-15 11:00:43 +0800

    moved pagination helper methods to layout_helper

--- pin-edu-sns/app/helpers/layout_helper.rb
+++ pin-edu-sns/app/helpers/layout_helper.rb
@@ -1,8 +1,25 @@
+# -*- coding: no-conversion -*-
 module LayoutHelper
   def page_buttons
     content_tag :div, :class => :buttons do
       yield LayoutWidget
     end
+  end
+
+  def list_pagination(collection)
+    str = content_tag(:span, :class => 'pagination-str') {current_displaying_items_str_for(collection)}
+    pagination = will_paginate(collection)
+    content_tag(:div, :class => 'page-paginate') {str + pagination}
+  end
+
+private
+
+  def current_displaying_items_str_for(collection)
+    offset = (params[:page] ? params[:page].to_i : 1) * Paginated::PERPAGE
+    start  = collection.blank? ? 0 : offset - Paginated::PERPAGE + 1
+    total  = collection.count
+    stop   = offset > total ? total : offset
+    "当前显示第#{start}-#{stop}项(共#{total}项)"
   end
 
   module LayoutWidget
