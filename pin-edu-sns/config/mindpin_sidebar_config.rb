66e0a880c899d92c05c8aa846ba01ddcef07f273
Kaid <kaid@kaid.me>
2012-11-23 17:57:05 +0800

    removed redundant navs

--- pin-edu-sns/config/mindpin_sidebar_config.rb
+++ pin-edu-sns/config/mindpin_sidebar_config.rb
@@ -178,14 +178,6 @@ MindpinSidebar::Base.config do
           controller :index, :only => :dashboard
         end
 
-        subnav :curriculum, :name => '我的课程表', :url => '/courses/curriculum' do
-          controller :courses, :only => :curriculum
-        end
-
-        subnav :courses, :name => '课程浏览', :url => '/courses' do
-          controller :courses, :only => :index
-        end
-
         subnav :students, :name => '学生浏览', :url => '/students' do
           controller :students, :only => :index
         end
