0eaa4126488d161d448ec450aa2c278e196f7421
ben7th <ben7th@sina.com>
2013-01-14 19:26:55 +0800

    时间线静态页面

--- pin-edu-sns/app/helpers/homeworks_helper.rb
+++ pin-edu-sns/app/helpers/homeworks_helper.rb
@@ -5,8 +5,6 @@ module HomeworksHelper
     timeline_open? ? button_template('切换到列表视图', nil) : button_template('切换到时间线视图', true)
   end
 
-private
-
   def button_template(text, status)
     link_to text, homeworks_path(:timeline => status), :class => 'homeworks-timeline-button'
   end
