60f88cc646f313d6ef4cd6a385580097e5bcfbb5
fushang318 <fushang318@gmail.com>
2013-01-15 21:16:32 +0800

    jlbalbkf

--- pin-edu-sns/lib/kaid_related/question_demo.rb
+++ pin-edu-sns/lib/kaid_related/question_demo.rb
@@ -7,12 +7,12 @@ DemoQuestionTitles = {
   "邹柏青" => "java内部是如何处理判断一个对象是否被实例化的？",
   "徐华建" => "Java 中的接口有什么作用？",
   "刘青"   => "什么是对象持久化，与数据序列化有何联系？",
-  "韩梅梅" => "C语言中参数向量argv问题",
+  "张无忌" => "C语言中参数向量argv问题",
   "彭思诗" => "java如何用byte[]构造BufferedImage,处理之后怎么再输出为byte[]?",
-  "韩梅梅2" => "java如不想么每次都判空 if(o !=null) 怎么做？",
+  "张无忌2" => "java如不想么每次都判空 if(o !=null) 怎么做？",
   "陶宣宣" => "关于电梯算法",
   "刘遂辉" => "有哪些算法的延伸阅读书籍可以推荐下吗？",
-  "韩梅梅3" => "Java里ClassName.this和this有什么不一样",
+  "张无忌3" => "Java里ClassName.this和this有什么不一样",
   "李华"   =>  "插值查找的问题"
 }
 
@@ -20,11 +20,11 @@ DemoQuestions = DemoQuestionTitles.map do |creator, title|
   q = DemoQuestion.new
   q.title = title
   q.creator = creator
-  q.creator = '韩梅梅' if creator.include? '韩梅梅'
+  q.creator = '张无忌' if creator.include? '张无忌'
   q.teacher_user = "张三丰"
   q.has_answered = [true, false][rand 2]
-  q.has_answered = true if creator.include? '韩梅梅'
+  q.has_answered = true if creator.include? '张无忌'
   q
 end.shuffle
 
-HMMQuestions = DemoQuestions.select {|q| q.creator == '韩梅梅'}.sort {|a, b| a.title <=> b.title}
+HMMQuestions = DemoQuestions.select {|q| q.creator == '张无忌'}.sort {|a, b| a.title <=> b.title}
