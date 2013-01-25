ae208a9ec9dc7866388ed1237ed42fdbd1103c9b
mykingla <kingla_pei@163.com>
2013-01-25 16:37:06 +0800

    Add validation of teaching_plan

--- pin-edu-sns/app/edu_models/course_pack/teaching_plan.rb
+++ pin-edu-sns/app/edu_models/course_pack/teaching_plan.rb
@@ -1,7 +1,12 @@
 class TeachingPlan < ActiveRecord::Base
-  
-  validates :title, :desc, :content, :presence => true
+  belongs_to :course
+
+  belongs_to :teacher_user,
+             :class_name  => 'User',
+             :foreign_key => :teacher_user_id
+
   has_many :chapters
   
-
+  validates :title, :desc, :course, :teacher_user, :semester_value, :presence => true
+  
 end
