b9b8a9e0ec0996bca42648c065f7e96177b495a2
fushang318 <fushang318@gmail.com>
2013-01-25 16:55:17 +0800

    chapters

--- pin-edu-sns/app/edu_models/course_pack/chapter.rb
+++ pin-edu-sns/app/edu_models/course_pack/chapter.rb
@@ -1,2 +1,15 @@
 class Chapter < ActiveRecord::Base
+  belongs_to :teaching_plan
+  belongs_to :creator, :class_name => 'User'
+  has_many :course_wares
+
+  validates :title, :presence => true
+  validates :teaching_plan, :presence => true
+  validates :creator, :presence => true
+
+  before_validation :set_chapter_title, :on => :create
+  def set_chapter_title
+    count = self.teaching_plan.chapters.count
+    self.title = "第 #{count+1} 章节"
+  end
 end
