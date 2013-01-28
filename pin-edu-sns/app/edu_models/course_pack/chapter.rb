712e0fc6cfccc4800d8c0f8dd25edcaf40f167cc
fushang318 <fushang318@gmail.com>
2013-01-28 15:02:52 +0800

    add course_ware create

--- pin-edu-sns/app/edu_models/course_pack/chapter.rb
+++ pin-edu-sns/app/edu_models/course_pack/chapter.rb
@@ -3,13 +3,12 @@ class Chapter < ActiveRecord::Base
   belongs_to :creator, :class_name => 'User'
   has_many :course_wares
 
-  validates :title, :presence => true
-  validates :teaching_plan, :presence => true
-  validates :creator, :presence => true
+  validates :title, :desc, :teaching_plan, :creator, :presence => true
 
-  before_validation :set_chapter_title, :on => :create
-  def set_chapter_title
+  before_validation :set_chapter_title_desc, :on => :create
+  def set_chapter_title_desc
     count = self.teaching_plan.chapters.count
     self.title = "第 #{count+1} 章节"
+    self.desc = "第 #{count+1} 章节描述"
   end
 end
