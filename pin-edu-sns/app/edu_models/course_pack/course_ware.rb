a25bfc36d598f4225eae4e54980419934730f5f5
Kaid <kaid@kaid.me>
2013-02-25 14:16:49 +0800

    made courseware preview work

--- pin-edu-sns/app/edu_models/course_pack/course_ware.rb
+++ pin-edu-sns/app/edu_models/course_pack/course_ware.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class CourseWare < ActiveRecord::Base
   belongs_to :media_resource
   belongs_to :chapter
@@ -8,6 +9,7 @@ class CourseWare < ActiveRecord::Base
   validates :title, :desc, :chapter, :creator, :presence => true
 
   before_validation :set_course_ware_title_desc, :on => :create
+
   def set_course_ware_title_desc
     count = self.chapter.course_wares.count
     self.title = "课件标题-#{count+1}"
@@ -29,4 +31,17 @@ class CourseWare < ActiveRecord::Base
     self.media_resource = media_resource
     self.save
   end
+
+  def content_kind
+    media_resource && self.media_resource.file_entity.content_kind
+  end
+
+  def url
+    media_resource && self.media_resource.file_entity.attach.url
+  end
+
+  def video_encode_status
+    media_resource && self.media_resource.file_entity.video_encode_status
+  end
+
 end
