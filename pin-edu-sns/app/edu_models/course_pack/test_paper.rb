50cfea9b2dbf7bebb16e0204f15e0927fdc89651
Kaid <kaid@kaid.me>
2013-03-04 13:08:02 +0800

    fixed a teaching_plan controller bug

--- pin-edu-sns/app/edu_models/course_pack/test_paper.rb
+++ pin-edu-sns/app/edu_models/course_pack/test_paper.rb
@@ -1,8 +1,18 @@
 class TestPaper < ActiveRecord::Base
-
   belongs_to :teaching_plan
-  belongs_to :student_user
+  belongs_to :student_user,
+             :class_name  => 'User',
+             :foreign_key => :student_user_id
 
   validates :teaching_plan , :presence => true
   validates :student_user , :presence => true
+
+  def select_random_questions
+    questions = self.teaching_plan.test_questions
+    10.times.inject({:left => questions, :selected => []}) do |acc, _|
+      question = acc[:left].dup.shuffle[0]
+      selected = acc[:selected].dup + [selected]
+      {:left => acc[:left].dup.reject {|q| q == question}, :selected => selected}
+    end[:selected]
+  end
 end
