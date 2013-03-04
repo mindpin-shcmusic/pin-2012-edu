9139a8040a99d21aa90e1ff481b5e2224cb7cec3
menxu <menxu_work@163.com>
2013-03-04 11:14:41 +0800

    udate test_papers

--- pin-edu-sns/db/migrate/20130304031056_create_test_papers.rb
+++ pin-edu-sns/db/migrate/20130304031056_create_test_papers.rb
@@ -1,7 +1,8 @@
 class CreateTestPapers < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table :test_papers do |t|
+      t.integer :teaching_plan_id
+      t.integer :student_user_id
+      t.timestamps
   end
 end
