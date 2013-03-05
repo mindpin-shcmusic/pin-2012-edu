1062cd620599145f2f3e62f0717278bb6998619d
ben7th <ben7th@sina.com>
2013-03-05 12:10:16 +0800

    调整学生导航

--- pin-edu-sns/app/helpers/course_helper.rb
+++ pin-edu-sns/app/helpers/course_helper.rb
@@ -71,4 +71,78 @@ module CourseHelper
     link_to name, href, :class => "item #{@cnav == sym ? 'current' : ''}"
   end
 
+  # ---------------
+  def page_head_cell(img_url, title, desc)
+    s1 = content_tag :div, :class => 'image' do
+      jimage img_url, :width => 80, :height => 80
+    end
+
+    s2 = content_tag :div, title, :class => 'ctitle'
+    s3 = content_tag :div, sanitize(desc), :class => 'cdesc'
+
+    content_tag :div, :class => 'page-head-cell' do
+      s1 + s2 + s3
+    end
+  end
+
+  def page_images_grid(image_hashs)
+    content_tag :div, :class => 'page-images-grid' do
+      s3 = image_hashs.map do |ih|
+        s1 = content_tag :div, :class => 'image' do
+          jimage ih[:url], :width => 200, :height => 150
+        end
+        s2 = content_tag :div, ih[:title], :class => 'title'
+        content_tag :div, :class => 'cell' do
+          s1 + s2
+        end
+      end.join('').html_safe
+    end
+  end
+
+  def page_course_resources(course)
+    hashs = course.chapters.map do |ch|
+      hash = Hash.new('')
+      hash[:url] = ''
+      hash[:title] = '无标题'
+
+      cw = ch.course_wares[0]
+      hash[:title] = ch.title
+
+      if !cw.blank?
+
+      end
+
+      hash
+    end
+  end
+
+  def page_cell_head_and_more(title, desc, more, url)
+    content_tag :div, :class => 'page-cell-head-and-more' do
+      s1 = content_tag :div, title, :class => 'title'
+      s2 = content_tag :div, desc, :class => 'desc'
+      s3 = content_tag :a, more, :href => url, :class => 'more'
+      s1 + s2 + s3
+    end
+  end
+
+  def page_cell_head_no_more(title, desc)
+    content_tag :div, :class => 'page-cell-head-and-more' do
+      s1 = content_tag :div, title, :class => 'title'
+      s2 = content_tag :div, desc, :class => 'desc'
+      s1 + s2
+    end
+  end
+
+  def page_user_small_cards(users)
+    content_tag :div, :class => 'page-user-cards' do
+      users.map do |user|
+        content_tag :div, :class => 'card' do
+          s1 = avatar user, :tiny
+          s2 = content_tag :div, user.real_name, :class => 'name'
+          s1 + s2
+        end
+      end.join('').html_safe
+    end
+  end
+
 end
